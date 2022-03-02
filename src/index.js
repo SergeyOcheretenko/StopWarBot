547d584f5d92fba23fe21b794d56e9ee623de742
SergeyOcheretenko <ocheretenko.s@gmail.com>
2022-03-02 19:52:12 +0200

    feat: add keyboard and commands

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
 const data = require('./json/CONFIG.json');
 const script = require('./json/script.json')
-const fs = require('fs')
 const persons = require('./json/people.json')
+const fs = require('fs')
 
 const bot = new Telegraf(data.token);
 
@@ -15,6 +15,7 @@ const savePerson = (id, username) => {
             flag = true;
             break;
         }
+        count++;
     }
 
     if (!flag) {
@@ -25,21 +26,47 @@ const savePerson = (id, username) => {
 }
 
 bot.start(async (ctx) => {
+    await ctx.reply(script.greeting, Markup.keyboard([['Видеообращение', 'Текстовое обращение']]).oneTime().resize())
     savePerson(ctx.message.chat.id, ctx.message.from.username);
-    await ctx.reply(script.greeting);
 });
 
 bot.help(async (ctx) => {
+    await ctx.reply(script.greeting, Markup.keyboard([['Видеообращение', 'Текстовое обращение']]).oneTime().resize())
     savePerson(ctx.message.chat.id, ctx.message.from.username);
-    await ctx.reply(script.greeting);
 })
 
-bot.command('chat_id', ctx => {
-    ctx.reply(ctx.message.chat.id);
+bot.command('chat_id', async ctx => {
+    await ctx.reply(ctx.message.chat.id);
 })
 
-bot.on('message', ctx => {
-    ctx.reply('Я не знаю что вы имели ввиду.\n\nНажимайте /help и я расскажу вам как действовать дальше.')
+bot.command('poster', async ctx => {
+    await ctx.reply(script.drive)
+    await ctx.reply(script.thanks)
+})
+
+bot.command('manual', async ctx => {
+    await ctx.reply(script.manual, Markup.keyboard([['Выбрать постер', 'Зачем это мне?']]).oneTime().resize())
+})
+
+bot.on('message', async ctx => {
+    const text = ctx.message.text;
+    
+    if (text === 'Видеообращение') {
+        await ctx.reply(script.youtube)
+    } else if (text === 'Текстовое обращение') {
+        await ctx.reply(script.first_speak);
+        await ctx.reply(script.second_speak);
+        await ctx.reply(script.third_speak, Markup.keyboard([['Выбрать постер', 'Инструкции', 'Зачем это мне?']]).oneTime().resize())
+    } else if (text === "Инструкции") {
+        await ctx.reply(script.manual, Markup.keyboard([['Выбрать постер', 'Зачем это мне?']]).oneTime().resize())
+    } else if (text === "Зачем это мне?") {
+        await ctx.reply(script.why, Markup.keyboard([['Выбрать постер', 'Инструкции']]).oneTime().resize())
+    } else if (text === 'Выбрать постер') {
+        await ctx.reply(script.drive)
+        await ctx.reply(script.thanks)
+    } else {
+        await ctx.reply(script.none)
+    }
 })
 
 bot.launch();
