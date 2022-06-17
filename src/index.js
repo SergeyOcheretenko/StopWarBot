f6d99a1e2ddc266c3d8f056c3f8d1e10f677ce10
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-06-17 18:26:25 +0300

    Refactoring
    + Delete people.json
    + Decompose message handling

--- src/index.js
+++ src/index.js
@@ -1,57 +1,28 @@
 const { Telegraf, Markup } = require('telegraf');
 const data = require('./json/CONFIG.json');
 const script = require('./json/script.json')
-const persons = require('./json/people.json')
 const fs = require('fs')
 
+const handleError = (err) => {
+    const adminId = 393017439;
+    bot.telegram.sendMessage(adminId, err.toString());
+};
+
 const bot = new Telegraf(data.token);
-
-const savePerson = (id, username) => {
-    try {
-        let count = 1;
-        let flag = false;
-        const keys = Object.keys(persons);
-        for (const key of keys) {
-            if (persons[key][0] === id) {
-                flag = true;
-                break;
-            }
-            count++;
-        }
-
-        if (!flag) {
-            persons[count] = [id, username];
-            const new_persons = JSON.stringify(persons);
-            fs.writeFileSync('./json/people.json', new_persons)
-        }   
-    } catch (e) {
-        bot.telegram.sendMessage(393017439, 'Ошибка');
-    } 
-}
 
 bot.start(async (ctx) => {
     try {
         await ctx.reply(script.greeting, Markup.keyboard([['Видеообращение', 'Текстовое обращение']]).oneTime().resize())
-        savePerson(ctx.message.chat.id, ctx.message.from.username);
     } catch (e) {
-        bot.telegram.sendMessage(393017439, 'Ошибка');
+        handleError(e);
     }
 });
-
-bot.help(async (ctx) => {
-    try {
-        await ctx.reply(script.greeting, Markup.keyboard([['Видеообращение', 'Текстовое обращение']]).oneTime().resize())
-        savePerson(ctx.message.chat.id, ctx.message.from.username);
-    } catch (e) {
-        bot.telegram.sendMessage(393017439, 'Ошибка');
-    }
-})
 
 bot.command('chat_id', async ctx => {
     try {
         await ctx.reply(ctx.message.chat.id);
     } catch (e) {
-        bot.telegram.sendMessage(393017439, 'Ошибка');
+        handleError(e);
     }
 })
 
@@ -60,7 +31,7 @@ bot.command('poster', async ctx => {
         await ctx.reply(script.drive)
         await ctx.reply(script.thanks)
     } catch (e) {
-        bot.telegram.sendMessage(393017439, 'Ошибка');
+        handleError(e);
     }
 })
 
@@ -68,33 +39,39 @@ bot.command('manual', async ctx => {
     try {
         await ctx.reply(script.manual, Markup.keyboard([['Выбрать постер', 'Зачем это мне?']]).oneTime().resize())
     } catch (e) {
-        bot.telegram.sendMessage(393017439, 'Ошибка');
+        handleError(e);
     }
-})
+});
+
+bot.hears('Видеообращение', async (ctx) => {
+    await ctx.replyWithVideo(
+	    'BAACAgIAAxkBAAIBi2IfuCKYW0v4OvdSr9SSA75oCAshAAIKFgAC4TT4SMK-RDLURhG0IwQ', 
+	    Markup.keyboard([['Выбрать постер', 'Инструкции', 'Зачем это мне?']])
+	    .oneTime().resize()
+    );
+});
+
+bot.hears('Текстовое обращение', async (ctx) => {
+    await ctx.reply(script.first_speak);
+    await ctx.reply(script.second_speak);
+    await ctx.reply(script.third_speak, Markup.keyboard([['Выбрать постер', 'Инструкции', 'Зачем это мне?']]).oneTime().resize());
+});
+
+bot.hears('Инструкции', async (ctx) => {
+    await ctx.reply(script.manual, Markup.keyboard([['Выбрать постер', 'Зачем это мне?']]).oneTime().resize());
+});
+
+bot.hears('Зачем это мне?', async (ctx) => {
+    await ctx.reply(script.why, Markup.keyboard([['Выбрать постер', 'Инструкции']]).oneTime().resize());
+});
+
+bot.hears('Выбрать постер', async (ctx) => {
+    await ctx.reply(script.drive);
+    await ctx.reply(script.thanks);
+});
 
 bot.on('message', async ctx => {
-    try { 
-        const text = ctx.message.text;
-        
-        if (text === 'Видеообращение') {
-            await ctx.replyWithVideo('BAACAgIAAxkBAAIBi2IfuCKYW0v4OvdSr9SSA75oCAshAAIKFgAC4TT4SMK-RDLURhG0IwQ', Markup.keyboard([['Выбрать постер', 'Инструкции', 'Зачем это мне?']]).oneTime().resize())
-        } else if (text === 'Текстовое обращение') {
-            await ctx.reply(script.first_speak);
-            await ctx.reply(script.second_speak);
-            await ctx.reply(script.third_speak, Markup.keyboard([['Выбрать постер', 'Инструкции', 'Зачем это мне?']]).oneTime().resize())
-        } else if (text === "Инструкции") {
-            await ctx.reply(script.manual, Markup.keyboard([['Выбрать постер', 'Зачем это мне?']]).oneTime().resize())
-        } else if (text === "Зачем это мне?") {
-            await ctx.reply(script.why, Markup.keyboard([['Выбрать постер', 'Инструкции']]).oneTime().resize())
-        } else if (text === 'Выбрать постер') {
-            await ctx.reply(script.drive)
-            await ctx.reply(script.thanks)
-        } else {
-            await ctx.reply(script.none)
-        }
-    } catch (e) {
-        bot.telegram.sendMessage(393017439, 'Ошибка');
-    }
+    await ctx.reply(script.none);
 })
 
 bot.launch();
